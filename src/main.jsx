286aa31d96aafef59e8594325876a65b8b52d120
prajwalbundhade <prajwalpro231@gmail.com>
2023-12-29 00:18:25 +0530

    redux toolkit added

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 
+
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
