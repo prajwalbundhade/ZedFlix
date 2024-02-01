8ce8977cc02a0cf0099fad3dd3d9353ef643df33
prajwalbundhade <prajwalpro231@gmail.com>
2024-02-01 14:35:05 +0530

    few changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useEffect } from 'react';
 
 import {fetchDataFromApi} from './utils/api';
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiconfiguration } from './store/homeSlice';
+
+getApiconfiguration
 
 function App() {
+  const dispatch = useDispatch();
+  const  {url} = useSelector((state) => state.home);
 
   useEffect(() => {
     apiTesting();
@@ -11,15 +17,20 @@ function App() {
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
+      dispatch(getApiconfiguration(res));
     }
     );
-  }  
+  }; 
 
   return (
-    <>
-
-    </>
-  )
+    
+      <div className='App'>
+        App
+        {url?.total_pages}   
+        
+      </div>
+  
+  );
 }
 
 export default App
