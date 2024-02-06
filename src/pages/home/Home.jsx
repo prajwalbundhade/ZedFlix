2bea6d6416129d233ff2be98200856f50f75fd87
prajwalbundhade <prajwalpro231@gmail.com>
2024-02-06 21:11:11 +0530

    home page updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import './style.scss'
 
 import HeroBanner from './heroBanner/HeroBanner'
-
+import Trending from "./trending/Trending";
 const Home = () => {
   return (
-    <div><HeroBanner /></div>
-  )
-}
+    <div className="homePage">
+            <HeroBanner />
+            <Trending />
+            
+        </div>
+  );
+};
 
 export default Home
