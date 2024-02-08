67aab1aac6a3d8e09886e79944166e0c99d326c3
prajwalbundhade <prajwalpro231@gmail.com>
2024-02-08 19:09:40 +0530

    package updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// vite.config.js
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/configuration-options/
+    },
+  },
 })
