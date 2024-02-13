c8763afbb52c1b9292907f2394ce6b368b7ead4d
prajwalbundhade <prajwalpro231@gmail.com>
2024-02-13 22:32:57 +0530

    details page updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,51 +1,76 @@
 import React from "react";
-import {
-    FaFacebookF,
-    FaInstagram,
-    FaTwitter,
-    FaLinkedin,
-} from "react-icons/fa";
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 
 import "./style.scss";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <ContentWrapper>
-                <ul className="menuItems">
-                    <li className="menuItem">Terms Of Use</li>
-                    <li className="menuItem">Privacy-Policy</li>
-                    <li className="menuItem">About</li>
-                    <li className="menuItem">Blog</li>
-                    <li className="menuItem">FAQ</li>
-                </ul>
-                <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
-                </div>
-                <div className="socialIcons">
-                    <span className="icon">
-                        <FaFacebookF />
-                    </span>
-                    <span className="icon">
-                        <FaInstagram />
-                    </span>
-                    <span className="icon">
-                        <FaTwitter />
-                    </span>
-                    <span className="icon">
-                        <FaLinkedin />
-                    </span>
-                </div>
-            </ContentWrapper>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              Terms Of Use
+            </a>{" "}
+          </li>
+          <li className="menuItem">
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              Privacy-Policy
+            </a>
+          </li>
+          <li className="menuItem">
+            {" "}
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              About
+            </a>{" "}
+          </li>
+          <li className="menuItem">
+            <a
+              target="_blank"
+              href="https://github.com/prajwalbundhade"
+              rel="noreferrer"
+            >
+              Doc
+            </a>
+          </li>
+        </ul>
+        <div className="infoText">
+          Zedflix is an immersive online platform designed for movie and TV show
+          enthusiasts. Powered by the TMDB API, Zedflix offers users a seamless
+          experience to explore, discover, and engage with their favorite movies
+          and TV shows.
+        </div>
+        <div className="socialIcons">
+          <a
+            target="_blank"
+            href="https://github.com/prajwalbundhade"
+            rel="noreferrer"
+            className="icon"
+          >
+            <FaGithub />
+          </a>
+          <a
+            target="_blank"
+            href="https://twitter.com/Mr_Prajwal06"
+            rel="noreferrer"
+            className="icon"
+          >
+            <FaTwitter />
+          </a>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/prajwal-bundhade-a53a76218/"
+            rel="noreferrer"
+            className="icon"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
 };
 
 export default Footer;
